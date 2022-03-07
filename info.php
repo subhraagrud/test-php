c952e327b791538af716429f58f6354934a28015
subhra <subhra.ghosh@agrud.com>
2022-03-07 17:06:02 +0530

    info.php

--- info.php
+++ info.php
@@ -3,4 +3,5 @@ echo "Hello World!";
 echo "Hello Sahab";
 echo "check server info";
 phpinfo ();
+echo "Something new changes have been done.";
 ?>
---
+++ subhrainfo.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "This ongoing year is ". date('Y');
+
+?>
